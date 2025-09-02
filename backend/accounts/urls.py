82a796c8492f8fa6580832f76281dcc8368fe1c2
ShifatiRabbi <rabbishifati@gmail.com>
2025-09-03 01:00:56 +0600

    appoinment codebase created

---
+++ backend/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # path('new_appointment/', create_appointment, name='create_appointment'),
+    # path('appointments/<int:appointment_id>/', get_appointment, name='get_appointment'),
+]
